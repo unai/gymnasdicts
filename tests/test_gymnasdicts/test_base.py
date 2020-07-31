a1697df016b7c663fefdf784c6c9d7ad40808809
gecBurton <g.e.c.burton@gmail.com>
2020-07-31 08:21:38 +0100

    moved parse_pointer fail to test_utils

--- tests/test_gymnasdicts/test_base.py
+++ tests/test_gymnasdicts/test_base.py
@@ -196,7 +196,6 @@ def test_select(payload, conditions, expected):
     [
         (2, {}, "unexpected payload type"),
         ([{}], {"a": "$.x"}, "'x' not found in '{}'"),
-        ([{}], {"a": "x&]"}, "Parse error at 1:2 near token ] (])"),
     ],
 )
 def test_select_fail(payload, pointers, message):
--- tests/test_gymnasdicts/test_utils.py
+++ tests/test_gymnasdicts/test_utils.py
@@ -39,3 +39,9 @@ def test_group_by(iterable, key, expected):
 )
 def test_parse_pointer(text, expected):
     assert parse_pointer(text) == expected
+
+
+def test_parse_pointer_fail():
+    with pytest.raises(ValueError) as value_error:
+        parse_pointer("x&]")
+    assert str(value_error.value) == "Parse error at 1:2 near token ] (])"
